630b8270c41f3e6421aad245dfca94a64ffb2731
manvithshetty507 <manvithshetty18@gmail.com>
2023-02-12 22:00:24 +0530

    4th

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,37 +7,5 @@ public class Main {
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
     }
-    /*
-    public class Boat implements WaterVehicle{
 
-        private String name;
-        private int capacity;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getCapacity() {
-            return capacity;
-        }
-
-        public void setCapacity(int capacity) {
-            this.capacity = capacity;
-        }
-
-        @Override
-        public String getVehicleName() {
-            return null;
-        }
-
-        @Override
-        public int getVehicleCapacity() {
-            return 0;
-        }
-    }
-    */
 }
